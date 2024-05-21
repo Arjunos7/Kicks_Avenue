31be737ae02df15aa6c98b95f58aa08ebffa8f3f
arjun <arjunos212@gmail.com>
2024-05-22 01:42:26 +0530

    commit

--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-class Customer(models.Model):
-    name=models.CharField(max_length=200)
-    address=models.TextField()
-    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
-    phone=models.CharField(max_length=10)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
